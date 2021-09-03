a9772753b72ffa5e687a8b8c21ed14aac8bd496e
Mirsario <me@mirsar.io>
2021-09-03 14:46:03 +0300

    Comments.

--- Src/Main.cs
+++ Src/Main.cs
@@ -9,6 +9,9 @@ namespace ExampleGame
 		{
 			DisplayName = "Example Game";
 
+			// DeferredRendering is currently the recommended rendering pipeline for 3D games with lighting.
+			// You may want to use ForwardRendering if you're making a 2D game, or make your own custom rendering pipeline.
+			// Note that shaders made for a specific pipeline are likely to misbehave in others.
 			Rendering.SetRenderingPipeline<DeferredRendering>();
 		}
 	}
