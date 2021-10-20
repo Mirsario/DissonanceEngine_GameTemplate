3671be3dfc35711a5f2342b61c49fd5363cae973
Mirsario <me@mirsar.io>
2021-10-20 18:55:39 +0300

    Updated engine.

--- Src/Common/StartSystem.cs
+++ Src/Common/StartSystem.cs
@@ -14,17 +14,21 @@ namespace ExampleGame.Common
 		{
 			var cameraEntity = World.CreateEntity();
 
-			cameraEntity.Set(new Camera(fov: 90f));
+			cameraEntity.Set(new Camera {
+				FieldOfView = 90
+			});
 			cameraEntity.Set(new Transform(-Vector3.UnitZ * 5f));
 
 			for (int i = 0; i < 5; i++) {
 				var cube = World.CreateEntity();
 
 				cube.Set(new Transform(Vector3.Zero));
-				cube.Set(new Light(Light.LightType.Point, Vector3.Normalize(new Vector3(Rand.Next(1f), Rand.Next(1f), Rand.Next(1f)))));
-				cube.Set(new MeshRenderer(PrimitiveMeshes.Cube, Resources.Find<Material>("TestCube")));
+				cube.Set(new MeshRenderer(PrimitiveMeshes.Cube, Assets.Get<Material>("Resources/Materials/TestCube.material")));
 				cube.Set(new SineMovement {
 					TimeOffset = i / 5f * MathHelper.TwoPI
+				});
+				cube.Set(new Light {
+					Color = Vector3.Normalize(new Vector3(Rand.Next(1f), Rand.Next(1f), Rand.Next(1f)))
 				});
 			}
 		}
