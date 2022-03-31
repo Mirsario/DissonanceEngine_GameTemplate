ef05a348a33d2c574f8789a3567a1b214d86e448
Mirsario <me@mirsar.io>
2022-03-31 04:29:54 +0300

    Modernized systems.

--- Src/Common/Meta/QuitSystem.cs
+++ Src/Common/Meta/QuitSystem.cs
@@ -5,16 +5,22 @@ using Dissonance.Engine.Input;
 namespace ExampleGame.Common.Meta
 {
 	/// <summary>
-	/// Quits the game whenever the escape key is pressed twice. Also controls cursor focus.
+	/// Quits the game whenever the escape key is pressed twice. Optionally controls cursor focus.
 	/// </summary>
 	[Callback<EndFixedUpdateCallback>]
-	public sealed class QuitSystem : GameSystem
+	public sealed partial class QuitSystem : GameSystem
 	{
-		protected override void Execute()
+		[Subsystem]
+		private partial void Update()
 		{
+			/*
 			if (InputEngine.GetMouseButton(MouseButton.Left)) {
-				//Screen.CursorState = CursorState.Disabled;
-			} else if (InputEngine.GetKeyDown(Keys.Escape)) {
+				Screen.CursorState = CursorState.Disabled;
+				return;
+			}
+			*/
+
+			if (InputEngine.GetKeyDown(Keys.Escape)) {
 				if (Screen.CursorState != CursorState.Normal) {
 					Screen.CursorState = CursorState.Normal;
 				} else {
